38d0d8d9790009fec550c4f25a13378edf5e85d4
Ella4040 <usouella@gmail.com>
2024-02-08 21:05:11 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./App.css";
+import Sidebar from './components/sidebar/Sidebar';
+import Services from './components/services/Services';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Blog from './components/blog/Blog';
+import Contact from './components/contact/Contact';
+import Testimonials from './components/testimonials/Testimonials'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <main className='main'>
+        <Home />
+        <About />
+        <Resume />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+    </>
+  );
+};
+
+export default App;
