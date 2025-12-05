d53996593547761ce3622c31a59a84f1f5ac84a5
Ella4040 <usouella@gmail.com>
2025-12-05 14:24:07 +0100

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './about.css';
 import Image from "../../assets/avatar-2.png";
 import AboutBox from './AboutBox';
-import Pdf from "../../assets/CV.Ella_English_20241005.pdf";
+import Pdf from "../../assets/CV_Ella_Dossou_English.pdf";
 
 const About = () => {
 
@@ -15,7 +15,7 @@ const About = () => {
         <div className="about__data grid">
           <div className="about__info">
             <p className="about__description">My name is Ella DOSSOU, a computer engineer passionate about technology, with over five years of experience and a Bachelor's degree +1 in digital solutions development and design. Professional, rigorous, and results-oriented, I have completed several projects that reflect my technical expertise and creativity. Curious and determined, I am always ready to learn, take on new challenges, and actively contribute to the creation of innovative web solutions.</p>
-            <a target="_blank" href="../../assets/CV.Ella_English_20241005.pdf" download="CV.Ella_English_20241005.pdf" class="btn">Download Resume</a>
+            <a target="_blank" href="../../assets/CV_Ella_Dossou_English.pdf" download="CV_Ella_Dossou_English.pdf" class="btn">Download Resume</a>
           </div>
 
           <div className="about__skills grid">
